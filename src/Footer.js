8f526e2f2e1279cf753c21cea0d07de2bdf9fe81
ryanfreeman320 <ryanfreeman320@gmail.com>
2025-07-27 23:50:26 -0600

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="flex gap-4 mt-2 md:mt-0">
           <a href="https://facebook.com" className="hover:text-blue-400">Facebook</a>
           <a href="https://instagram.com" className="hover:text-pink-400">Instagram</a>
-          <a href="mailto:info@nipponmotors.com" className="hover:text-blue-300">Email</a>
+          <a href="mailto:info@nipponmotors.org" className="hover:text-blue-300">Email</a>
         </div>
       </div>
     </footer>
