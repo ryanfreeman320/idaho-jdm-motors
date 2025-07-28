1a8886619c71f605c5a793021db6891131553383
ryanfreeman320 <ryanfreeman320@gmail.com>
2025-07-27 21:07:34 -0600

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,8 @@ export default function Contact() {
       <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
       <p className="mb-2">📍 Boise, Idaho</p>
       <p className="mb-2">📧 contact@nipponmotors.com</p>
+      <p className="mb-2">Shoot us a message for additional vehicle details and photos</p>
+      <p className="mb-2">Vehicle viewing by appointment only</p>
     </div>
   );
 }
